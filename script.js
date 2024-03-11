75945c680b51dd2001d05bba787efa83ac13d6ee
Manh <07mandao@skola.boras.se>
2024-03-11 18:49:03 +0100

    added so it check if someone wins verticaly

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let gameBoard = (function creatGameBoard(){
     return {row1 : [" ", " ", " "],
             row2 : [" ", " ", " "],
             row3 : [" ", " ", " "],};
-})();
+})(); 
 
 function createPlayer(name){
     let MakeMove = (x, y, player)=>{
@@ -15,17 +15,17 @@ function createPlayer(name){
         }
         const x_cordinate = x;
         const y_cordinate = y;
-        if (y_cordinate === 1 && gameBoard.row1[x_cordinate - 1] === " "){
+        if (y_cordinate === 1 && gameBoard.row1[x_cordinate - 1] ===  " "){
             gameBoard.row1[x_cordinate - 1] = playerPeice;
         }
-        else if (y_cordinate === 2 && gameBoard.row1[x_cordinate - 1] === " "){
+        else if (y_cordinate === 2 && gameBoard.row2[x_cordinate - 1] === " "){
             gameBoard.row2[x_cordinate - 1] = playerPeice;
         }
-        else if (y_cordinate === 3 && gameBoard.row1[x_cordinate - 1] === " "){
+        else if (y_cordinate === 3 && gameBoard.row3[x_cordinate - 1] === " "){
             gameBoard.row3[x_cordinate - 1] = playerPeice;
         }
         else{
-            console.log("That spot its occupied");
+            console.log("The spot its ocupied");
         }
         checkWinner();
     }
@@ -33,17 +33,25 @@ function createPlayer(name){
 }
 
 function checkWinner(){
-    gameArray = [gameBoard.row1, gameBoard.row2, gameBoard.row3];
-    someoneHasWon = false;
-    let arrayIndex = 0;
-    let verticalArray = [];
-    for (arrays of gameArray){
+    let gameArray = [gameBoard.row1, gameBoard.row2, gameBoard.row3];
+    let someoneHasWon = false;
+    let verticalArrays = [];
+    for (let arrays of gameArray){
         if (arrays.join("") === "xxx" || arrays.join("") === "ooo"){
             someoneHasWon = true;
         }
-        else if (verticalArray.join("") === "xxx" || verticalArray.join("") === "ooo")
-        verticalArray.push(arrays[arrayIndex]);
-        arrayIndex ++;
+    }
+    for (let i = 0; i < 3; i++){
+        let verticalArray = [];
+        for (let arrays of gameArray){
+            verticalArray.push(arrays[i]) 
+        }
+        verticalArrays.push(verticalArray);
+    }
+    for (let arrays of verticalArrays){
+        if (arrays.join("") === "xxx" || arrays.join("") === "ooo"){
+            someoneHasWon = true;
+        }
     }
     if(someoneHasWon === true){
         console.log("Someone has won!");
